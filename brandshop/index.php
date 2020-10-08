3c123a06638c7fd5bc747ea4d135431b1bd42f0e
Kim Valera <kim_valera@bk.ru>
2020-10-08 22:29:07 +0500

    Internet shop

---
+++ brandshop/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="styles/site.css" rel="stylesheet">
+    <script src="scripts/jquery.js"></script>
+    <script src="scripts/site.js"></script>
+    <title>Онлайн магазин</title>
+</head>
+<body>
+<header>
+    <div id="headerInside">
+        <div id="logo"></div>
+        <div id="companyName">Brand</div>
+        <div id="navWrap">
+            <a href="/">
+                Главная
+            </a>
+            <a href="index.php?page=shop">
+                Магазин
+            </a>
+        </div>
+    </div>
+</header>
+
+<div id="content">
+
+    <?php
+    $goods = [
+        [
+            'id' => 1,
+            'name' => 'Iphone',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc',
+            'img' => '/images/goods/iphone.jpg',
+            'price' => '2000 $'
+        ],
+        [
+            'id' => 2,
+            'name' => 'HTC',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc',
+            'img' => '/images/goods/htc.jpg',
+            'price' => '1200 $'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Samsung',
+            'desc' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc',
+            'img' => '/images/goods/samsung.jpg',
+            'price' => '1400 $'
+        ],
+    ];
+
+    $page = $_GET['page'];
+
+    if (!isset($page)) {
+        require('templates/main.php');
+    } elseif ($page == 'shop') {
+        require('templates/shop.php');
+    } elseif ($page == 'product') {
+        $id = $_GET['id'];
+        $good = [];
+        foreach ($goods as $product) {
+            if ($product['id'] == $id) {
+                $good = $product;
+                break;
+            }
+        }
+        require('templates/openedProduct.php');
+    }
+?>
+
+</div>
+
+<footer>
+    <div id="footerInside">
+
+        <div id="contacts">
+            <div class="contactWrap">
+                <img src="images/envelope.svg" class="contactIcon">
+                info@brandshop.ru
+            </div>
+            <div class="contactWrap">
+                <img src="images/phone-call.svg" class="contactIcon">
+                8 800 555 00 00
+            </div>
+            <div class="contactWrap">
+                <img src="images/placeholder.svg" class="contactIcon">
+                Москва, пр-т Ленина, д. 1 офис 304
+            </div>
+        </div>
+
+        <div id="footerNav">
+            <a href="/">Главная</a>
+            <a href="index.php?page=shop">Магазин</a>
+        </div>
+
+        <div id="social">
+            <img class="socialItem" src="images/vk-social-logotype.svg">
+            <img class="socialItem" src="images/google-plus-social-logotype.svg">
+            <img class="socialItem" src="images/facebook-logo.svg">
+        </div>
+
+        <div id="copyrights">&copy; Brand</div>
+    </div>
+</footer>
+
+</body>
+</html>
